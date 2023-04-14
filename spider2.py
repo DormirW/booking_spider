37223cedd4922d6cdd482e2dec330d26a003c577
dormir <dormirw@qq.com>
2023-04-14 22:59:41 +0800

    加入多线程并行处理

--- spider2.py
+++ spider2.py
@@ -5,6 +5,7 @@ from datashape import null
 import time
 from DAO import getCursor
 import openpyxl
+import threading
 
 MAX_RETRIES = 3
 
@@ -28,7 +29,7 @@ def get_res(url, header):
     return None
 
 
-if __name__ == '__main__':
+def crawl(places):
     header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.69",
         "Cookie": "cnfunco=1; cors_js=1; bkng_sso_session=e30; bkng_sso_ses=e30; _pxvid=a9278cf5-c273-11ed-abc2-61766a736e6a; aliyungf_tc=d3885ada5956aae4854d7f2d90a5450565aebdd37dba3f5cc48c6e44aa587d12; pxcts=1eaefc4b-c2e6-11ed-be83-6e44527a546c; _px3=b00ae55e3e40e14b8459a531f519a748224447082cb9522b158167ac90fcbc32:ycsVtiW3puL9qwm64tJrDhRLcMmNu37y58fRzDnsNhsjkJU1Yv2p6xd38D6fqbKcCRPFqc44cMKRkt/uc8SlVA==:1000:58kZIf28YEBeDrApusYwPF7y+MsyMeDTAFiahHCcQE83YEgoH5SL1V0GmyAaQtWI9f4lijGxnRT3NXG8vFOEqs/0RwLmBGuBxAqwpkKJdr3Eh6njLy4IzMPvDfKS/X7MFiJkQ4Ixc6baLIh5GeqFyXjFenr1nRAwnG5YbbZ1Gw4nXGOIWUdYklIOwYCezOYThUmkkXGCUIbHaDKxXMx/YA==; _pxde=e350cb609d3e9380ec12b46d348cda305235ff04a281c24c4c35d03b9703feab:eyJ0aW1lc3RhbXAiOjE2Nzg4NjA5MTYwNzMsImZfa2IiOjAsImlwY19pZCI6W119; acw_tc=781bad4d16788610098562653e3e457a574af3ef4a11e0de50339346fc8e86; OptanonConsent=isGpcEnabled=0&datestamp=Wed+Mar+15+2023+14%3A16%3A52+GMT%2B0800+(%E4%B8%AD%E5%9B%BD%E6%A0%87%E5%87%86%E6%97%B6%E9%97%B4)&version=6.22.0&isIABGlobal=false&hosts=&consentId=9ed25a52-ac48-498a-9154-02a2277c150b&interactionCount=1&landingPath=NotLandingPage&AwaitingReconsent=false&groups=C0001%3A1%2CC0002%3A0%2CC0004%3A0&implicitConsentCountry=GDPR&implicitConsentDate=1678803715087; bkng=11UmFuZG9tSVYkc2RlIyh9Yaa29%2F3xUOLbiKbS0JOgDBKd%2F%2B1hfoh7rsSN%2BxB95HtADwNYBs8pGf2lSb2tl%2FzFOhcqAGvO9287BuvqnpxJp%2FYvSeB0JnGKuKXONr6PaKaTqjIKtIFNTFi0c07sxHNaiLMvvFr3De4Csoj82Qf60AK8434Pk8LrQZQKiiVeiU4idAcAQtO2Reo%3D"
@@ -44,14 +45,6 @@ if __name__ == '__main__':
     #           "资阳市", "贵州", "云南", "西安市", "铜川市", "宝鸡市", "咸阳市", "渭南市", "延安市", "汉中市", "榆林市",
     #           "安康市", "商洛市", "甘肃", "青海", "台湾", "内蒙古",
     #           "广西", "西藏", "宁夏", "新疆", "北京", "天津", "上海", "重庆", "香港", "澳门"]
-    places = ["铁岭市", "朝阳市", "葫芦岛市", "江苏", "浙江", "安徽",
-              "福建", "江西", "山东", "郑州市", "开封市", "洛阳市", "平顶山市", "安阳市", "鹤壁市", "新乡市", "焦作市",
-              "濮阳市", "许昌市", "漯河市", "三门峡市", "南阳市", "商丘市", "信阳市", "周口市", "驻马店市",
-              "湖北", "湖南", "广东", "海南", "成都市", "自贡市", "攀枝花市", "泸州市", "德阳市", "绵阳市", "广元市",
-              "遂宁市", "内江市", "乐山市", "南充市", "眉山市", "宜宾市", "广安市", "达州市", "雅安市", "巴中市",
-              "资阳市", "贵州", "云南", "西安市", "铜川市", "宝鸡市", "咸阳市", "渭南市", "延安市", "汉中市", "榆林市",
-              "安康市", "商洛市", "甘肃", "青海", "台湾", "内蒙古",
-              "广西", "西藏", "宁夏", "新疆", "北京", "天津", "上海", "重庆", "香港", "澳门"]
     # 建立数据库链连接
     con, cur = getCursor()
     for place in places:
@@ -239,25 +232,24 @@ if __name__ == '__main__':
     # 提交请求，关闭数据库
     con.commit()
     con.close()
-    # 保存数据
-    workbook = openpyxl.Workbook()
-    worksheet = workbook.active
-    sheet_header = ["id", "link", "name", "description", "area", "country", "province", "city", "location", "image1",
-                    "image2", "image3"]
 
-    # 写入表头
-    for col, h in enumerate(sheet_header, 1):
-        cell = worksheet.cell(row=1, column=col)
-        cell.value = h
 
-    # 写入数据
-    for row, rd in enumerate(elem_list, 2):
-        for col, cd in rd.items():
-            cell = worksheet.cell(row=row, column=sheet_header.index(col) + 1)
-            cell.value = cd
+if __name__ == '__main__':
+    places = [["安徽",
+              "福建"], ["江西", "山东"], ["郑州市", "开封市", "洛阳市", "平顶山市", "安阳市", "鹤壁市", "新乡市", "焦作市",
+              "濮阳市", "许昌市", "漯河市", "三门峡市", "南阳市", "商丘市", "信阳市", "周口市", "驻马店市",
+              "湖北"], ["湖南"], ["广东"], ["海南"], ["成都市", "自贡市", "攀枝花市", "泸州市", "德阳市", "绵阳市", "广元市",
+              "遂宁市", "内江市", "乐山市", "南充市", "眉山市", "宜宾市", "广安市", "达州市", "雅安市", "巴中市",
+              "资阳市"], ["贵州", "云南"], ["西安市", "铜川市", "宝鸡市", "咸阳市", "渭南市", "延安市", "汉中市", "榆林市",
+              "安康市", "商洛市", "甘肃"], ["青海", "台湾", "内蒙古"],
+              ["广西", "西藏", "宁夏"], ["新疆", "天津"], ["北京"], ["上海"], ["重庆"], ["香港", "澳门"]]
+    threads = []
+    for place in places:
+        t = threading.Thread(target=crawl, args=(place,))
+        t.start()
+        threads.append(t)
 
-    # 保存到xlsx
-    workbook.save(filename="data.xlsx")
+    for t in threads:
+        t.join()
 
-    with open("data.json", "w") as f:
-        f.write(json.dumps(elem_list, indent=4, separators=(',', ': ')))
+    print("All threads finished")
